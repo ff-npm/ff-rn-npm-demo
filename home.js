27ab687f3f0b02a34bd6cf15408853cd6843577b
dxj <hanyuan_company_dxj@dxj.com>
2019-08-15 09:59:40 +0800

    media测试方法

--- home.js
+++ home.js
@@ -1,6 +1,12 @@
+/*
+ * @description: 
+ * @author: dxj
+ * @Date: 2019-07-12 17:52:52
+ */
 
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, NativeModules } from "react-native";
+import { StyleSheet, Text, View, Button, NativeModules, ScrollView } from "react-native";
+let arr = Array.from(new Array(30).keys());
 class home extends Component {
 
     static navigationOptions = () => ({
@@ -9,14 +15,21 @@ class home extends Component {
 
     render() {
         return (
-            <View>
+            <ScrollView>
                 <View style={{marginTop:20}}>
                     <Button onPress={()=>this.props.navigation.navigate("qrcode")} title="扫码"></Button>
                 </View>
                 <View style={{marginTop:20}}>
                     <Button onPress={()=>this.props.navigation.navigate("toast")} title="toast"></Button>
                 </View>
-            </View>
+                {
+                    arr.map((item)=>(
+                        <View key={item} style={{marginTop:20}}>
+                            <Button style={{marginVertical: 20}} onPress={()=>{NativeModules.rnmedia.media(item)}} title={item.toString()}></Button>
+                        </View>
+                    ))
+                }
+            </ScrollView>
         );
     }
 
